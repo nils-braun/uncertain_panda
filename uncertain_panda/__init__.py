4b1e9184d9572116f0075bff04892f98195bc7ab
Nils Braun <nils.braun@kit.edu>
2018-11-11 19:15:02 +0100

    Export pandas as pandas

--- uncertain_panda/__init__.py
+++ uncertain_panda/__init__.py
@@ -4,4 +4,6 @@ __version__ = "0.1.0"
 from .utils.pandas_utils import add_uncertainty_accessors
 add_uncertainty_accessors()
 
-from pandas import *
+from .uncertainties.calculation import UncertaintyMode
+
+import pandas
